d795626d381e11abf8377166e575b14890ba964b
Kvazar3S273 <ba2h007@gmail.com>
2021-09-15 23:38:04 +0300

    one

---
+++ AutoPart/frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hello, Mars!</h1>
+    <h2>Houston, we have a problem</h2>
+    </>
+
+  );
+}
+
+export default App;
