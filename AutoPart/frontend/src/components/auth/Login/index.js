e2dc360fccec5b40973c9efe06928773b0b5c237
Kvazar3S273 <ba2h007@gmail.com>
2021-10-07 00:19:19 +0300

    add validation Yup

--- AutoPart/frontend/src/components/auth/Login/index.js
+++ AutoPart/frontend/src/components/auth/Login/index.js
@@ -1,34 +1,45 @@
 import React, { useState } from 'react';
 import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 import MyTextInput from '../../common/MyTextInput';
 
 const LoginPage = () => {
     return (
         <div className="row">
-        <div className="offset-md-3 col-md-6">
-        <h1 className="text-center">Вхід на сайт</h1>
-            <Formik
-                initialValues={{
-                    email: '',
-                }}
-                onSubmit={(values) => {
-                    console.log("values submit", values)
-                }}>
-                <Form>
-                    <MyTextInput
-                        label="Пошта"
-                        name="email"
-                        type="text"
-                    />
-                    <MyTextInput
-                        label="Пароль"
-                        name="password"
-                        type="password"
-                    />
-                    <input type="submit" className="btn btn-sucsess" value="Вхід"></input>
-                </Form>
-            </Formik>
-        </div>
+            <div className="offset-md-3 col-md-6">
+                <h1 className="text-center">Вхід на сайт</h1>
+                <Formik
+                    initialValues={{
+                        email: '',
+                        password: ''
+                    }}
+                    validationSchema={Yup.object({
+                        email: Yup.string()
+                            .email("Не коректно вказана пошта")
+                            .required("Вкажіть пошту"),
+                        password: Yup.string()
+                        .required("Введіть пароль")
+                        .min(5, "Пароль занадто короткий, потрібно не менше 5 символів")
+                        .matches(/[a-zA-Z]/, "Пароль повинен містити хоча б одну латинську літеру")
+                    })}
+                    onSubmit={(values) => {
+                        console.log("values submit", values)
+                    }}>
+                    <Form>
+                        <MyTextInput
+                            label="Пошта"
+                            name="email"
+                            type="text"
+                        />
+                        <MyTextInput
+                            label="Пароль"
+                            name="password"
+                            type="password"
+                        />
+                        <input type="submit" className="btn btn-success btn-lg" value="Вхід"></input>
+                    </Form>
+                </Formik>
+            </div>
         </div>
     )
 }
--- AutoPart/frontend/src/components/common/MyTextInput.js
+++ AutoPart/frontend/src/components/common/MyTextInput.js
@@ -1,12 +1,18 @@
 import {useField} from 'formik';
+import classNames from 'classnames';
 
 const MyTextInput = ({label, ...props}) => {
     const [field, meta] = useField(props);
     return (
         <div className="mb-3">
             <label htmlFor={props.id || props.name} className="form-label">{label}</label>
-            <input className="form-control" {...field} {...props}/>
-        </div>
+            <input className={classNames("form-control",
+                { "is-invalid": meta.touched && meta.error },
+                { "is-valid": meta.touched && !meta.error },
+            )} {...field} {...props} />
+            {(meta.touched && meta.error) &&
+                <div className="invalid-feedback">{meta.error} </div>}
+            </div>
     );
 };
 
