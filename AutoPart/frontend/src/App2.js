d93c5bb9bb534e5776526c587ebe6124160d0149
Kvazar3S273 <ba2h007@gmail.com>
2021-11-07 23:10:02 +0200

    add layout

--- AutoPart/frontend/src/App2.js
+++ AutoPart/frontend/src/App2.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
-import Header from './components/header';
-import RegisterPage from './components/auth/Register';
-import LoginPage from './components/auth/Login';
-import HomePage from './components/home';
-import GetUserList from './components/userlist';
-import DeleteUser from './components/userlist/Delete';
-import EditUser from './components/userlist/Edit';
+// import Header from './components/header';
+// import RegisterPage from './components/auth/Register';
+// import LoginPage from './components/auth/Login';
+// import HomePage from './components/home';
+// import GetUserList from './components/userlist';
+// import DeleteUser from './components/userlist/Delete';
+// import EditUser from './components/userlist/Edit';
+const DefaultLayout = React.lazy(() => import('./components/containers/DefaultLayout'));
 
 function App2() {
 
   return (
     <>
-      <Header />
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Switch>
+          <Route path="/" name="Default" render={props => <DefaultLayout {...props} />} />
+        </Switch>
+      </Suspense>
+      {/* <Header />
       <div className="container">
         <Switch>
           <Route exact path="/home"><HomePage /></Route>
@@ -28,7 +34,7 @@ function App2() {
           <Route exact path="/edit/:id" render={({ match }) => <EditUser match={match} />}></Route>
           <Route exact path="/delete/:id" render={({ match }) => <DeleteUser match={match} />}></Route>
         </Switch>
-      </div>
+      </div> */}
     </>
 
   )
---
+++ AutoPart/frontend/src/components/containers/DefaultLayout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../../header";
+
+export default props => (
+    <>
+        <Header />
+        <div className="container">
+            {props.children}
+        </div>
+    </>
+)
